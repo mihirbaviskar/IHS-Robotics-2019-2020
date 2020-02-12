fe408c3409f194e46a3b110f37d780bc1c650bc2
sweeneyngo <44306479+sweeneyngo@users.noreply.github.com>
2020-02-11 19:53:56 -0800

    Create clock_set.c

---
+++ Kar Choon's Bizarre Adventures/Solutions/clock_set.c
@@ -0,0 +1,34 @@
+#include <kipr/botball.h>
+#include<stdbool.h>
+
+void clock_set();
+
+int main()
+{	
+	clock_set();
+	return 0;
+}
+
+void clock_set() {
+	int min = 0; int hour = 0;
+	while (!digital(0)) {
+		if (left_button() || right_button()) { //if any button is pressed
+			if (left_button()) { //if left button is pressed
+				while (left_button()) { msleep(1); } //breaks when the button isn't pressed anymore
+				hour++; //add an hour
+				if(hour>12) { hour = 0; } //if the hour is greater than 12, reset back to 0
+			}
+			if (right_button()) { //if right button is pressed
+				while (right_button()) { msleep(1); } //breaks when the button isn't pressed anymore
+				min++; //add a minute
+				if(min>59) { min = 0; } //if minute is greater than 59, reset back to 0
+			}
+			console_clear(); //clear the screen
+			if (hour<10) { printf("0"); } //print out values
+			printf("%i:", hour);
+			if (min<10) { printf("0");}
+			printf("%i", min);
+		}
+	} // continues loop
+	printf("\nYour time is set."); 
+}
