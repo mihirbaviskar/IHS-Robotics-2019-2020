2d1c8e6f39e4e29d53aeb171b7f271056413c200
sweeneyngo <44306479+sweeneyngo@users.noreply.github.com>
2020-02-27 13:34:17 -0800

    Update and rename amazingAlignPerp.c to AlignPerp.c

--- Small Bot the spaceship/AlignPerp.c
+++ Small Bot the spaceship/AlignPerp.c
@@ -1,15 +1,19 @@
 #include <kipr/botball.h>
 #include <stdio.h>
+
 #define LEFTWHEEL 3
 #define RIGHTWHEEL 2
 #define KPR 0.01
 #define KPL 0.01
 #define GRAYMAX 2300
 #define GRAYMIN 1900
+#declare OVERLINE 1
+#declare UNDERLINE 2
+
 void align(int side, double ltarget, double rtarget);
 
 
-void align(int side, double ltarget, double rtarget) //ltarget: 825 , rtarget: 825
+void align(int side, double ltarget, double rtarget) 
 {
     double currentleft = analog(1);
     double currentright = analog(2);
@@ -17,10 +21,11 @@ void align(int side, double ltarget, double rtarget) //ltarget: 825 , rtarget: 8
     double errorright = currentright-rtarget;
     double lproportional;
     double rproportional;
+
     int start_time = seconds();
-    int tiempo = seconds() - start_time;
+    int timer = seconds() - start_time;
     
-    if(side == 1){		//OVERLINE
+    if(side == OVERLINE){
         
      while(!((analog(1) > (ltarget-50) && analog(1) < (ltarget+50)) && (analog(2) > (rtarget-50) && analog(2) < (ltarget+50))))                 
 		//not on grey
@@ -28,43 +33,52 @@ void align(int side, double ltarget, double rtarget) //ltarget: 825 , rtarget: 8
 	 currentleft = analog(1);
 	 currentright = analog(2);
 	 errorleft = currentleft-ltarget;
-     errorright = currentright-rtarget;
+     	 errorright = currentright-rtarget;
 	 lproportional = KPL * errorleft;
 	 rproportional = KPR * errorright;
-            
+         
+	 /*
 	 printf("lproportional: %lf\n",lproportional);
 	 printf("rproportional: %lf\n",rproportional);
+	 */
 	 motor(LEFTWHEEL,lproportional);
-     motor(RIGHTWHEEL,rproportional);
-     msleep(10);
-     printf("Left: \t %i \n",analog(1));
-     printf("Right: \t %i \n",analog(2)); 
-     tiempo = seconds() - start_time;
-         if(tiempo>=10){break;} 
-        }
+         motor(RIGHTWHEEL,rproportional);
+         msleep(10);
+	 /*
+         printf("Left: \t %i \n",analog(1));
+         printf("Right: \t %i \n",analog(2));
+     	*/
+         timer = seconds() - start_time;
+         if(timer>=10){break;} 
+         }
      }
 
-     if(side == 2){		//BELOWLINE
+     if(side == UNDERLINE){
         
          while(!((analog(1) > (ltarget-50) && analog(1) < (ltarget+50)) && (analog(2) > (rtarget-50) && analog(2) < (ltarget+50))))                
              //not on grey
          {
-	 		currentleft = analog(1);
-	 		currentright = analog(2);
+	    currentleft = analog(1);
+	    currentright = analog(2);
             errorleft = ltarget-currentleft;
             errorright = rtarget-currentright;
             lproportional = KPL * errorleft;
             rproportional = KPR * errorright;
+	    /*
             printf("lproportional: %lf\n",lproportional);
             printf("rproportional: %lf\n",rproportional);
+	    */
+		 
             motor(LEFTWHEEL, lproportional);
             motor(RIGHTWHEEL, rproportional);
             msleep(10);
+            /*
             printf("Left: \t %i \n",analog(1));
             printf("Right: \t %i \n",analog(2));
-            tiempo = seconds()-start_time;
-            printf("\t TIEMPOOOOO: %i\n",tiempo);
-             if(tiempo>=10){break;} 
+	    */
+            timer = seconds()-start_time;
+            //printf("\t TIEMPOOOOO: %i\n",tiempo);
+            if(timer>=10){break;} 
          }
     }
     ao();
