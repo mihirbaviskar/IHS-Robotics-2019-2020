d8c49f47ab3b79a4bb106d3fb798ea247ca50a52
justan1004 <57971292+justan1004@users.noreply.github.com>
2020-03-03 18:41:36 -0800

    Update actualthingpart1.c

--- Small Bot the spaceship/actualthingpart1.c
+++ Small Bot the spaceship/actualthingpart1.c
@@ -7,11 +7,9 @@
 #include <Claw.h>
 #include <FloorPlan.h>
 
-#define FORWARD true
-#define BACKWARD false
-
 /* March 4th Plan
-** - 
+** - Test the whole goingOnATrip()
+** - Perfect goingOnATrip()
 */
 
 int main() {
@@ -19,14 +17,7 @@ int main() {
     enable_servos();
     startPos();
     msleep(1000);
-    //theWholeShebang();
-    //move_to(-100,-100,2000);
-    move_to(50,-100,1110);
-    ao();
-    moveSweeper(70,465);
-    set_servo_position(0,1350);
-    msleep(5000);
-    animalCrossing(2,BACKWARD);
+    goingOnATrip();
     disable_servos();
     return 0;
 }
@@ -57,13 +48,13 @@ void animalCrossing(int tape, bool direction){
     while(counter < tape){
         if(direction){
             while(TOPHAT < BLACK){
-                move_to(100,75,1);
+                move_to(100,71,1);
                 ao();
                 printf("%d\n",counter);
             }
             ao();
             while(TOPHAT > WHITE){
-                move_to(100,75,1);
+                move_to(100,71,1);
                 ao();
                 printf("%d\n",counter);
             }
@@ -87,19 +78,20 @@ void animalCrossing(int tape, bool direction){
 }
 
 void goingOnATrip(){
-    move_to(40,-50,2000);
+    move_to(50,-100,1110);
     ao();
-    /*moveSweeper(70,400);
-    msleep(100);
-    animalCrossing(2, BACKWARD); //push boxes together
+    moveSweeper(70,465);
+    set_servo_position(0,1350);
+    msleep(5000);
+    animalCrossing(2,BACKWARD);
+    //code below has NOT been tested w/ code above
     msleep(500);
     animalCrossing(1,FORWARD); //adjust
-    theWholeShebang();*/
+    theWholeShebang();
 }
 
 /*void inOurFavoriteRocketShip(){
-	set_servo_position(SPACESHIPHEAD,700);
-    moveSweeper(70,690);
+    set_servo_position(SPACESHIPHEAD,700);
     animalCrossing(1, FORWARD); //foward
     move_to(50,100,90*8.5); //turn left, TEST VAL
     pid_one_sensor_forwards_till_black(BLACK,75,5000,1); //!! wrong analog !!
@@ -111,6 +103,7 @@ void zoomingThroughTheSky(){
     set_servo_position(SPACESHIPHEAD, 100); //tilt head
     move_to(-100,-100,1000);//TEST VAL
     set_servo_position(SPACESHIPARM, 900); //lower spaceship
+    moveSweeper(70,690); //lower sweeper
     pid_one_sensor_forwards(BLACK,75,3000,1,0,0); //TEST VAL
 }*/
 
@@ -132,25 +125,17 @@ void clapClaw(int speed, int finalClaw){
 }
 
 void theActualThing(){
-    clapClaw(20,560); //test a slower speed
+    clapClaw(20,560);
 }
 
 void theWholeShebang(){
     moveSweeper(70,400);//DOWN
   	msleep(500);
-    /*clapClaw(20,800);
-    msleep(1000);
-    set_servo_position(SWEEPERCLAW,1350);
-    msleep(1000);
-    clapClaw(10,600);
-    msleep(1000);
-    set_servo_position(SWEEPERCLAW,1350);
-    msleep(1000);*/
     thread sweeperPickUp;
 	sweeperPickUp = thread_create(theActualThing);
 	thread_start(sweeperPickUp);
     msleep(3000);
-    moveSweeper(30,1200); //make it lower
+    moveSweeper(30,1200);
     thread_destroy(sweeperPickUp);
 }
 
