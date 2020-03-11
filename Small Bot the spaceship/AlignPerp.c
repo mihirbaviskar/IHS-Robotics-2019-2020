f0a88c0ea9ccfeb2ec54676e4e559f383d619b77
Kar Choon Chin <48036135+KC-oWo@users.noreply.github.com>
2020-03-10 17:21:47 -0700

    Update AlignPerp.c

--- Small Bot the spaceship/AlignPerp.c
+++ Small Bot the spaceship/AlignPerp.c
@@ -1,37 +1,35 @@
 #include <kipr/botball.h>
 #include <stdio.h>
 
-#define LEFTWHEEL 3
-#define RIGHTWHEEL 2
-#define KPR 0.01
-#define KPL 0.01
+#define LEFTWHEEL 0
+#define RIGHTWHEEL 1
+#define KPR 0.025
+#define KPL 0.025
 #define GRAYMAX 2300
 #define GRAYMIN 1900
-#declare OVERLINE 1
-#declare UNDERLINE 2
 
-void align(int side, double ltarget, double rtarget);
+void align(int side, int lport, int rport, double ltarget, double rtarget);
 
 
-void align(int side, double ltarget, double rtarget) 
+void align(int side, int lport, int rport, double ltarget, double rtarget) 
 {
-    double currentleft = analog(1);
-    double currentright = analog(2);
+    double currentleft = analog(lport);
+    double currentright = analog(rport);
     double errorleft = currentleft-ltarget;
     double errorright = currentright-rtarget;
     double lproportional;
     double rproportional;
-
+	printf("Hello\n");
     int start_time = seconds();
     int timer = seconds() - start_time;
     
-    if(side == OVERLINE){
-        
-     while(!((analog(1) > (ltarget-50) && analog(1) < (ltarget+50)) && (analog(2) > (rtarget-50) && analog(2) < (ltarget+50))))                 
-		//not on grey
+    if(side == 1){
+     printf("Yes\n");
+     while(!((analog(lport) > (ltarget-50) && analog(lport) < (ltarget+50)) && (analog(rport) > (rtarget-50) && analog(rport) < (rtarget+50))))          
      {
-	 currentleft = analog(1);
-	 currentright = analog(2);
+     printf("Yes1\n");
+	 currentleft = analog(lport);
+	 currentright = analog(rport);
 	 errorleft = currentleft-ltarget;
      	 errorright = currentright-rtarget;
 	 lproportional = KPL * errorleft;
@@ -44,22 +42,22 @@ void align(int side, double ltarget, double rtarget)
 	 motor(LEFTWHEEL,lproportional);
          motor(RIGHTWHEEL,rproportional);
          msleep(10);
-	 /*
-         printf("Left: \t %i \n",analog(1));
-         printf("Right: \t %i \n",analog(2));
-     	*/
+	 
+         printf("Left: \t %i \n",analog(lport));
+         printf("Right: \t %i \n",analog(rport));
+     	
          timer = seconds() - start_time;
-         if(timer>=10){break;} 
+         if(timer>=5){break;} 
          }
      }
 
-     if(side == UNDERLINE){
-        
-         while(!((analog(1) > (ltarget-50) && analog(1) < (ltarget+50)) && (analog(2) > (rtarget-50) && analog(2) < (ltarget+50))))                
+     if(side == 2){
+         printf("No\n");
+         while(!((analog(lport) > (ltarget-50) && analog(lport) < (ltarget+50)) && (analog(rport) > (rtarget-50) && analog(rport) < (rtarget+50))))                
              //not on grey
          {
-	    currentleft = analog(1);
-	    currentright = analog(2);
+	    currentleft = analog(lport);
+	    currentright = analog(rport);
             errorleft = ltarget-currentleft;
             errorright = rtarget-currentright;
             lproportional = KPL * errorleft;
@@ -78,7 +76,7 @@ void align(int side, double ltarget, double rtarget)
 	    */
             timer = seconds()-start_time;
             //printf("\t TIEMPOOOOO: %i\n",tiempo);
-            if(timer>=10){break;} 
+            if(timer>=5){break;} 
          }
     }
     ao();
