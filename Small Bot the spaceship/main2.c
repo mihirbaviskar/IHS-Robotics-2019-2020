dec238fb0f2d2b7ba971315afb98bfa5b8e14e46
Viha123 <47764272+Viha123@users.noreply.github.com>
2020-03-11 17:35:23 -0700

    Update main2.c

--- Small Bot the spaceship/main2.c
+++ Small Bot the spaceship/main2.c
@@ -1,5 +1,5 @@
 #include <kipr/botball.h>
-#include <movement.h>
+#include <Movement.h>
 #include <PID.h>
 #include <AlignPerp.h>
 /* Ports */
@@ -26,52 +26,75 @@
 #define LFLINE analog(0)
 #define RTLINE analog(1)
 #define TARGET 1900
-#define WHITE analog(3) < TARGET
-#define BLACK analog(3) > TARGET
+#define WHITE analog(3) < 3200
+#define BLACK analog(3) > 3200
+// ET sensor
+#define THRESHOLD 2900
 int main() {
     enable_servos();
-    organize_cubes();
-    while(analog(1)<TARGET){
+   // organize_cubes();
+    while(analog(1)<TARGET+1000){
     	pid_one_sensor_backwards(TARGET, 50, 0.01, LEFTSIDE);
     }
-    
+    ao();
    	msleep(250);
     mav(LEFTMOTOR, -500); //tilt robot left to fit the cubes in the zone
     mav(RIGHTMOTOR, -1000);
-    msleep(1800);
+    msleep(2000);
     ao();
     
-    /*
-    while (analog(3) < TARGET) { //moves back until front sensor touches black
-    	mav(LEFTMOTOR, 750);
+    
+  /* while (analog(3) < TARGET) { //moves back until front sensor touches black
+    	mav(LEFTMOTOR, 750);  //stuff to comment later on
     	mav(RIGHTMOTOR, 750);
     }
     */
-   	mav(LEFTMOTOR, 750);
-    mav(RIGHTMOTOR, 750);
-    msleep(500);
+   	mav(LEFTMOTOR, 400);  // going 'back' to 
+    mav(RIGHTMOTOR, 950);
+    msleep(650);
     ao();
-   
+    
     move_servo(SWEEPERRAISE, 50, SWEEPMAX+200); //raises claw to go over ramp
     while (WHITE) {
+        printf("Moving from white to black.\n");
         mav(LEFTMOTOR, -750); 
         mav(RIGHTMOTOR, 750);
     }
     ao();
+    msleep(2000);
     while (BLACK) {
+        printf("Moving from black to white, forward.\n");
+		mav(LEFTMOTOR,  750); 
+        mav(RIGHTMOTOR, 750);
+    }
+    ao();
+	msleep(2000);
+
+    
+    while (WHITE) {
+        printf("Moving from white to black.\n");
         mav(LEFTMOTOR, -750); 
         mav(RIGHTMOTOR, 750);
     }
     ao();
-    
-    mav(LEFTMOTOR, -750); 
-    mav(RIGHTMOTOR, 750);
-    msleep(1000);
+    msleep(2000);
+    while (BLACK) {
+        printf("Moving from black to white, turn.\n");
+        mav(LEFTMOTOR, -750); 
+        mav(RIGHTMOTOR, 750);
+    }
     ao();
-    
     align(OVERLINE, 1, 0, TARGET, TARGET); //align robot parallel to black line
+    ao(); 
+    mav(LEFTMOTOR, -750); 
+    mav(RIGHTMOTOR, -750);
+    msleep(300);
     ao();
-    pid_one_sensor_forwards(TARGET, 600, 5, LEFTSIDE, 3);;
+    align(UNDERLINE, 1, 0, TARGET, TARGET); //align robot parallel to black line
+    
+    while(analog(4) < THRESHOLD){
+    pid_one_sensor_forwards(2900, 600, 0.001, LEFTSIDE, 3);
+    }
     ao();
     //pid_one_sensor_backwards(TARGET, 50, 1.8, RIGHTSIDE); //change distance
     /*while(analog(1)<TARGET){
